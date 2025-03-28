9809d6231fe7075db786fd3447ae0d2b0ecef636
Andreas Filser <filser-a@gmx.de>
2025-03-28 23:32:54 +0100

    flipbook integration

--- courses/ggplot_flipbook/_gif_create.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-pagedown::chrome_print(input = "./courses/ggplot_flipbook/index.html")
-# then create gif as follows
-magick::image_read_pdf(path = "./courses/ggplot_flipbook/index.pdf", density = 100) %>% # create images
-  magick::image_write_gif(path = "./courses/ggplot_flipbook/featured.gif", delay = .4) # images to gif
-
-
-# https://github.com/quarto-dev/quarto-cli/discussions/1840
